837029f6980ccbc853ac3b0a1846e1208306caf8
Alexander <64682381+Alexander2990@users.noreply.github.com>
2022-03-23 21:56:56 +0500

    Add files via upload

---
+++ HackerNews/HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Александр on 23.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
